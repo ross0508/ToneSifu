805a7a2a23894bd41419dfa2310a9ee0cb24550c
ross0508 <rossdavidson02@gmail.com>
2025-01-09 18:30:17 +0000

    Start navbar and hero section

---
+++ tone-sifu/src/Hero/Hero.tsx
@@ -0,0 +1,10 @@
+import './Hero.css'
+
+export default function Hero() {
+  return (
+    <div className="hero-container">
+      <h1 className="hero-title">Tone Sifu</h1>
+      <p className="hero-subtitle">Tone training platform</p>
+    </div>
+  )
+}
---
+++ tone-sifu/src/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import './Navbar.css'
+export default function Navbar() {
+  return (
+    <ul className="navbar-container">
+        <button className="navbutton navbutton-primary">Home</button>
+        <button className="navbutton navbutton-secondary">Profile</button>
+    </ul>
+  )
+}
