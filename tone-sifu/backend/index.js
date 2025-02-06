0c59fbd85d1838dfff519ff29bccc496e0bac913
ross0508 <rossdavidson02@gmail.com>
2025-02-06 14:59:47 +0000

    Added get n random cantonese words API endpoint

--- tone-sifu/backend/index.js
+++ tone-sifu/backend/index.js
@@ -10,8 +10,18 @@ app.listen(PORT, () => {
     console.log('Running on localhost:' + PORT)
 })
 
+
+// GET word by id
+
 app.get('/words/cantonese/:id', async (req, res) => {
     const { id } = req.params
     const word = await pool.query("SELECT * FROM cantoneseword WHERE word_id = $1", [id])
     res.json(word.rows)
+})
+
+// GET n random words
+app.get('/words/cantonese/random/:n', async (req, res) => {
+    const { n } = req.params
+    const words = await pool.query('SELECT * FROM cantoneseword ORDER BY RANDOM() LIMIT $1', [n]);
+    res.json(words.rows)
 })
