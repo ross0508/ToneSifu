670033b40b8938167ee58d972f34315c25cf0c0a
ross0508 <rossdavidson02@gmail.com>
2025-02-08 12:45:00 +0000

    Display score for each tone on results screen

--- tone-sifu/frontend/src/Results/Results.tsx
+++ tone-sifu/frontend/src/Results/Results.tsx
@@ -2,12 +2,25 @@ import './Results.css'
 
 export default function Results({ score, total, questionLog, testStateSetter }) {
   
-console.log(questionLog)
+console.log(score)
+console.log(total)
+console.log(total[1])
 
   return (
     <>
       <div className='score-container'>
         <div className='score-display'>Score: {score[0]} / {total[0]}</div>
+        <div className='score-display'>{total.map((tone, i) => {
+          return (
+            <>
+              {i !== 0 && total[i] !== 0 ? (
+                <div>Tone {i}: {score[i]} / {tone}</div>
+              ) : (
+                <></>
+              )}
+            </>
+          );
+        })}</div>
         <button className='retry-button' onClick={() => testStateSetter(0)}>Retry</button>
       </div>
       <div className='results-container'>
