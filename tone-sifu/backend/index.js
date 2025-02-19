5d57ee61bb1bc6f1b668f7846e9c571800ebb014
ross0508 <rossdavidson02@gmail.com>
2025-02-19 23:32:58 +0000

    Added time period dropdown on profile for progress graph

--- tone-sifu/backend/index.js
+++ tone-sifu/backend/index.js
@@ -1,157 +1,153 @@
-const PORT = 8080
-const cors = require('cors');
-const pool = require("./db")
+const PORT = 8080;
+const cors = require("cors");
+const pool = require("./db");
 
-const express = require('express')
-app = express()
+const express = require("express");
+app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 app.listen(PORT, () => {
-    console.log('Running on localhost:' + PORT)
-})
-
-
-
+  console.log("Running on localhost:" + PORT);
+});
 
 // MANDARIN
 
 // GET word by id
 
-app.get('/words/cmn/:id', async (req, res) => {
-    const { id } = req.params
-    const word = await pool.query("SELECT * FROM mandarinword WHERE word_id = $1", [id])
-    res.json(word.rows)
-})
-
+app.get("/words/cmn/:id", async (req, res) => {
+  const { id } = req.params;
+  const word = await pool.query(
+    "SELECT * FROM mandarinword WHERE word_id = $1",
+    [id]
+  );
+  res.json(word.rows);
+});
 
 // GET n random words
 
-app.get('/words/cmn/random/:n', async (req, res) => {
-    const { n } = req.params
-    const toneArray = req.query.tones.map(Number) // Convert tones sent in params to array of numbers
-    const words = await pool.query('SELECT * FROM mandarinword WHERE tone = ANY($1::int[]) ORDER BY RANDOM() LIMIT $2', [toneArray, n])
-    res.json(words.rows)
-})
-
-
-
+app.get("/words/cmn/random/:n", async (req, res) => {
+  const { n } = req.params;
+  const toneArray = req.query.tones.map(Number); // Convert tones sent in params to array of numbers
+  const words = await pool.query(
+    "SELECT * FROM mandarinword WHERE tone = ANY($1::int[]) ORDER BY RANDOM() LIMIT $2",
+    [toneArray, n]
+  );
+  res.json(words.rows);
+});
 
 // CANTONESE
 
 // GET word by id
 
-app.get('/words/yue/:id', async (req, res) => {
-    const { id } = req.params
-    const word = await pool.query("SELECT * FROM cantoneseword WHERE word_id = $1", [id])
-    res.json(word.rows)
-})
-
+app.get("/words/yue/:id", async (req, res) => {
+  const { id } = req.params;
+  const word = await pool.query(
+    "SELECT * FROM cantoneseword WHERE word_id = $1",
+    [id]
+  );
+  res.json(word.rows);
+});
 
 // GET n random words
 
-app.get('/words/yue/random/:n', async (req, res) => {
-    const { n } = req.params
-    const toneArray = req.query.tones.map(Number) // Convert tones sent in params to array of numbers
-    const words = await pool.query('SELECT * FROM cantoneseword WHERE tone = ANY($1::int[]) ORDER BY RANDOM() LIMIT $2', [toneArray, n])
-    res.json(words.rows)
-})
-
-
-
+app.get("/words/yue/random/:n", async (req, res) => {
+  const { n } = req.params;
+  const toneArray = req.query.tones.map(Number); // Convert tones sent in params to array of numbers
+  const words = await pool.query(
+    "SELECT * FROM cantoneseword WHERE tone = ANY($1::int[]) ORDER BY RANDOM() LIMIT $2",
+    [toneArray, n]
+  );
+  res.json(words.rows);
+});
 
 // USERS
 
 // Create user
 
-app.post('/users/:user_id', async (req, res) => {
-    const { user_id } = req.params
-    
-    try {
-        const userResult = await pool.query(
-            'INSERT INTO users (user_id, exp) VALUES ($1, $2) RETURNING user_id, exp',
-            [user_id, 0]
-        );
+app.post("/users/:user_id", async (req, res) => {
+  const { user_id } = req.params;
 
-        const { user_id: userId, exp } = userResult.rows[0]
+  try {
+    const userResult = await pool.query(
+      "INSERT INTO users (user_id, exp) VALUES ($1, $2) RETURNING user_id, exp",
+      [user_id, 0]
+    );
 
-        res.status(201).json({ user_id: userId, exp })
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ error: 'Database error' })
-    }
+    const { user_id: userId, exp } = userResult.rows[0];
+
+    res.status(201).json({ user_id: userId, exp });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Database error" });
+  }
 });
-
 
 // Add EXP
 
-app.put('/users/:user_id', async (req, res) => {
-    const { user_id } = req.params
-    const { expAdded, language } =  req.body
-    if (language == 'yue')
-        try {
-            const userResult = await pool.query(
-                'UPDATE users SET exp = exp + $1, exp_yue = exp_yue + $1 WHERE user_id = $2 RETURNING user_id, exp',
-                [expAdded, user_id]
-            );
+app.put("/users/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+  const { expAdded, language } = req.body;
+  if (language == "yue")
+    try {
+      const userResult = await pool.query(
+        "UPDATE users SET exp = exp + $1, exp_yue = exp_yue + $1 WHERE user_id = $2 RETURNING user_id, exp",
+        [expAdded, user_id]
+      );
 
-            const { user_id: userId, exp } = userResult.rows[0]
+      const { user_id: userId, exp } = userResult.rows[0];
 
-            res.status(201).json({ user_id: userId, exp })
-        } catch (error) {
-            console.error(error)
-            res.status(500).json({ error: 'Database error' })
-        }
-    if (language == 'cmn')
-        try {
-            const userResult = await pool.query(
-                'UPDATE users SET exp = exp + $1, exp_cmn = exp_cmn + $1 WHERE user_id = $2 RETURNING user_id, exp',
-                [expAdded, user_id]
-            );
+      res.status(201).json({ user_id: userId, exp });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Database error" });
+    }
+  if (language == "cmn")
+    try {
+      const userResult = await pool.query(
+        "UPDATE users SET exp = exp + $1, exp_cmn = exp_cmn + $1 WHERE user_id = $2 RETURNING user_id, exp",
+        [expAdded, user_id]
+      );
 
-            const { user_id: userId, exp } = userResult.rows[0]
+      const { user_id: userId, exp } = userResult.rows[0];
 
-            res.status(201).json({ user_id: userId, exp })
-        } catch (error) {
-            console.error(error)
-            res.status(500).json({ error: 'Database error' })
-        }
-})
-
+      res.status(201).json({ user_id: userId, exp });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Database error" });
+    }
+});
 
 // Get EXP
 
-app.get('/users/:user_id', async (req, res) => {
-    const { user_id } = req.params
+app.get("/users/:user_id", async (req, res) => {
+  const { user_id } = req.params;
 
-    try {
-        const userResult = await pool.query(
-            'SELECT exp, exp_yue, exp_cmn FROM users WHERE user_id = $1', [user_id]
-        )
-        res.json(userResult.rows)
-    } catch {
-        console.error(error)
-        res.status(500).json({ error: 'Database error' })
-    }
-})
-
-
-
+  try {
+    const userResult = await pool.query(
+      "SELECT exp, exp_yue, exp_cmn FROM users WHERE user_id = $1",
+      [user_id]
+    );
+    res.json(userResult.rows);
+  } catch {
+    console.error(error);
+    res.status(500).json({ error: "Database error" });
+  }
+});
 
 // PERFORMANCE LOGS
 
-
 // Add performance log MANDARIN
 
-app.post('/log/cmn/:user_id', async (req, res) => {
-    const { user_id } = req.params
-    const score = req.query.score.map(Number) // Convert scores sent in params to array of numbers
-    const total = req.query.total.map(Number) // Same for total answered
-    const date = new Date().toISOString().split('T')[0];
+app.post("/log/cmn/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+  const score = req.query.score.map(Number); // Convert scores sent in params to array of numbers
+  const total = req.query.total.map(Number); // Same for total answered
+  const date = new Date().toISOString().split("T")[0];
 
-    try {
-        const userResult = await pool.query(
-            `INSERT INTO user_performance_cmn (total_correct, total_answered, date, user_id) VALUES ($1, $2, $3, $4) ON CONFLICT (user_id, date) 
+  try {
+    const userResult = await pool.query(
+      `INSERT INTO user_performance_cmn (total_correct, total_answered, date, user_id) VALUES ($1, $2, $3, $4) ON CONFLICT (user_id, date) 
     DO UPDATE SET 
     total_correct = array(
         SELECT unnest(user_performance_cmn.total_correct) + unnest(EXCLUDED.total_correct)
@@ -159,27 +155,26 @@ app.post('/log/cmn/:user_id', async (req, res) => {
     total_answered = array(
         SELECT unnest(user_performance_cmn.total_answered) + unnest(EXCLUDED.total_answered)
     )`,
-            [score, total, date, user_id]
-        );
-        res.status(201)
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ error: 'Database error' })
-    }
-})
-
+      [score, total, date, user_id]
+    );
+    res.status(201);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Database error" });
+  }
+});
 
 // Add performance log CANTONESE
 
-app.post('/log/yue/:user_id', async (req, res) => {
-    const { user_id } = req.params;
-    const score = req.query.score.map(Number); // Convert scores sent in params to array of numbers
-    const total = req.query.total.map(Number); // Same for total answered
-    const date = new Date().toISOString().split('T')[0];
+app.post("/log/yue/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+  const score = req.query.score.map(Number); // Convert scores sent in params to array of numbers
+  const total = req.query.total.map(Number); // Same for total answered
+  const date = new Date().toISOString().split("T")[0];
 
-    try {
-        const userResult = await pool.query(
-            `INSERT INTO user_performance_yue (total_correct, total_answered, date, user_id) 
+  try {
+    const userResult = await pool.query(
+      `INSERT INTO user_performance_yue (total_correct, total_answered, date, user_id) 
             VALUES ($1, $2, $3, $4) 
             ON CONFLICT (user_id, date) 
             DO UPDATE SET 
@@ -189,57 +184,57 @@ app.post('/log/yue/:user_id', async (req, res) => {
                 total_answered = array(
                     SELECT unnest(user_performance_yue.total_answered) + unnest(EXCLUDED.total_answered)
                 )`,
-            [score, total, date, user_id]
-        );
-        res.status(201).send('User performance logged');
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Database error' });
-    }
+      [score, total, date, user_id]
+    );
+    res.status(201).send("User performance logged");
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Database error" });
+  }
 });
-
 
 // Get last 30 days MANDARIN
 
-app.get('/log/cmn/:user_id', async (req, res) => {
-    const { user_id } = req.params;
-    try {
-        const result = await pool.query(
-            `SELECT * 
-             FROM user_performance_cmn 
+app.get("/log/cmn/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+  const timePeriod = req.query.timePeriod;
+  console.log(timePeriod);
+  try {
+    const result = await pool.query(
+      `SELECT * 
+             FROM user_performance_cmn
              WHERE user_id = $1 
-             AND TO_DATE(date, 'YYYY-MM-DD') >= CURRENT_DATE - INTERVAL '30 days' 
+             AND TO_DATE(date, 'YYYY-MM-DD') >= CURRENT_DATE - make_interval(days => $2) 
              ORDER BY date DESC`,
-            [user_id]
-        );
-        
-        res.json(result.rows); // Send the result back as JSON
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Database error' });
-    }
-});
+      [user_id, timePeriod]
+    );
 
+    res.json(result.rows); // Send the result back as JSON
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Database error" });
+  }
+});
 
 // Get last 30 days CANTONESE
 
-app.get('/log/yue/:user_id', async (req, res) => {
-    const { user_id } = req.params;
-    const timePeriod = req.query.timePeriod
-    console.log(timePeriod)
-    try {
-        const result = await pool.query(
-            `SELECT * 
+app.get("/log/yue/:user_id", async (req, res) => {
+  const { user_id } = req.params;
+  const timePeriod = req.query.timePeriod;
+  console.log(timePeriod);
+  try {
+    const result = await pool.query(
+      `SELECT * 
              FROM user_performance_yue 
              WHERE user_id = $1 
              AND TO_DATE(date, 'YYYY-MM-DD') >= CURRENT_DATE - make_interval(days => $2) 
              ORDER BY date DESC`,
-            [user_id, timePeriod]
-        );
-        
-        res.json(result.rows); // Send the result back as JSON
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Database error' });
-    }
+      [user_id, timePeriod]
+    );
+
+    res.json(result.rows); // Send the result back as JSON
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Database error" });
+  }
 });
--- tone-sifu/frontend/src/Profile/Profile.tsx
+++ tone-sifu/frontend/src/Profile/Profile.tsx
@@ -1,17 +1,17 @@
-import FullNavbar from "../FullNavbar/FullNavbar"
-import { useAuth0 } from "@auth0/auth0-react"
-import './Profile.css'
+import FullNavbar from "../FullNavbar/FullNavbar";
+import { useAuth0 } from "@auth0/auth0-react";
+import "./Profile.css";
 import LineGraph from "./LineGraph/LineGraph";
-import axios from 'axios'
-import { useState, useEffect } from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
 import ExpBar from "./ExpBar/ExpBar";
 
 export default function Profile() {
-
   const { user, isAuthenticated } = useAuth0();
-  const [language, setLanguage] = useState('cmn')
-  const [logData, setLogData] = useState([])
-  const [expData, setExpData] = useState([])
+  const [language, setLanguage] = useState("cmn");
+  const [logData, setLogData] = useState([]);
+  const [expData, setExpData] = useState([]);
+  const [timePeriod, setTimePeriod] = useState(30);
 
   useEffect(() => {
     const getExp = async () => {
@@ -20,16 +20,16 @@ export default function Profile() {
           method: "GET",
           url: `http://localhost:8080/users/${user.sub}`,
         });
-        setExpData(response.data)
+        setExpData(response.data);
       } catch (error) {
-        console.error("Error getting exp data", error)
+        console.error("Error getting exp data", error);
       }
-    }
+    };
 
     if (isAuthenticated) {
-      getExp()
+      getExp();
     }
-  }, [isAuthenticated])
+  }, [isAuthenticated]);
 
   useEffect(() => {
     const getLogs = async () => {
@@ -38,32 +38,33 @@ export default function Profile() {
           method: "GET",
           url: `http://localhost:8080/log/${language}/${user.sub}`,
           params: {
-            timePeriod: 60
-          }
+            timePeriod: timePeriod,
+          },
         });
-        setLogData(response.data)
+        setLogData(response.data);
       } catch (error) {
-        console.error("Error getting log data", error)
+        console.error("Error getting log data", error);
       }
-    }
+    };
 
     if (isAuthenticated) {
-      getLogs()
+      getLogs();
     }
-  }, [isAuthenticated, language]) // Get logs from database again when language setting is changed
+  }, [isAuthenticated, language, timePeriod]); // Get logs from database again when language or timePeriod changes
 
-  
+  const handleDropdown = (e) => {
+    setTimePeriod(e.target.value);
+  };
 
   if (!isAuthenticated || logData.length === 0 || expData.length === 0) {
     return (
       <>
         <FullNavbar />
         <div className="profile-container">
-            
           <h1>loading...</h1>
         </div>
       </>
-    )
+    );
   }
 
   return (
@@ -73,24 +74,45 @@ export default function Profile() {
         <div className="profile-information-container">
           <h1>Name</h1>
           <p>{user.name}</p>
-          {expData.length > 0 && 
-          <>
-            <p>Total Exp: {expData[0].exp}</p>
-          </>
-          }
+          {expData.length > 0 && (
+            <>
+              <p>Total Exp: {expData[0].exp}</p>
+            </>
+          )}
         </div>
         <div className="graph-container">
           <div className="graph-button-container">
             <LineGraph language={language} logData={logData}></LineGraph>
-            {language == 'yue' && <button className='graph-language-button' onClick={() => setLanguage('cmn')}>Cantonese</button>}
-            {language == 'cmn' && <button className='graph-language-button' onClick={() => setLanguage('yue')}>Mandarin</button>}
+            {language == "yue" && (
+              <button
+                className="graph-language-button"
+                onClick={() => setLanguage("cmn")}
+              >
+                Cantonese
+              </button>
+            )}
+            {language == "cmn" && (
+              <button
+                className="graph-language-button"
+                onClick={() => setLanguage("yue")}
+              >
+                Mandarin
+              </button>
+            )}
           </div>
-          {language == 'cmn' && <h1>Exp: {expData[0].exp_cmn}</h1>}
-          {language == 'cmn' && <ExpBar exp={expData[0].exp_cmn}/>}
-          {language == 'yue' && <h1>Exp: {expData[0].exp_yue}</h1>}
-          {language == 'yue' && <ExpBar exp={expData[0].exp_yue}/>}
+          {language == "cmn" && <h1>Exp: {expData[0].exp_cmn}</h1>}
+          {language == "cmn" && <ExpBar exp={expData[0].exp_cmn} />}
+          {language == "yue" && <h1>Exp: {expData[0].exp_yue}</h1>}
+          {language == "yue" && <ExpBar exp={expData[0].exp_yue} />}
         </div>
+
+        <label>Time Period:</label>
+        <select value={timePeriod} onChange={handleDropdown}>
+          <option value={7}>Week</option>
+          <option value={30}>Month</option>
+          <option value={365}> Year</option>
+        </select>
       </div>
     </>
-  )
+  );
 }
